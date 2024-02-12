d576526d82e595b5f7c660f5d37d46ed89dbcf84
Joeweath <jweathingtonsr@gmail.com>
2024-02-12 16:45:00 -0600

    step 6-7

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const resultDiv = document.getElementById('result');
 
 let text = textInput.addEventListener('input', (e) => text = e.target.value)
 console.log(text)
-checkButton.addEventListener('click', (e) => {
-    console.log(text)
-    if(text === undefined) {
+checkButton.addEventListener('click', () => {
+    if(text === undefined || text.length === 0) {
         alert("Please input a value")
-    } else if(text.toUpperCase() === "A") result.innerText = "A is a palindrome"
+    } else palindromeChecker(text)
 })
 
+
+const palindromeChecker = (str) => {
+    console.log(`Here is the word: ${str.toUpperCase()}`, str.split('').reverse().join('').toUpperCase())
+    if(str.toUpperCase() === str.split('').reverse().join('').toUpperCase()){
+
+        console.log(`${str} is a palindrome`)
+    } else console.log(`${str} is a NOT palindrome`)
+}
